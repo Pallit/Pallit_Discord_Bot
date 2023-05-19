c40d0994bad5b20a531450a990dd6cf9beef2d36
Niyaz Valeev <niaz-valeev@mail.ru>
2023-05-19 21:45:04 +0300

    feat: добавлен функционал по заходу бота на голосовой канал

--- init.py
+++ init.py
@@ -1,13 +1,55 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 import requests
 import validators
 from urllib.parse import urlparse
+import youtube_dl
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
 
 links = []
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 
 @bot.command(name='add')
@@ -34,5 +76,24 @@ async def links_list(ctx):
     await ctx.channel.send(links)
 
 
+@bot.command(name='play')
+async def play(ctx):
+    if not ctx.message.author.voice:
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
 
-bot.run('MTA5NDcwMzYyODMxNDQ5Mjk2OA.Gzgt1e.NCWYC9g5zeicubJz8SZE2FIGsoSoSdhPw5xRIw')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    bot_client = discord.utils.get(bot.voice_clients)
+    if before.channel is None:
+        return
+    if bot_client is None:
+        return
+    if len(before.channel.members) == 1 and bot_client.is_connected():
+        await bot_client.disconnect(force=True)
+
+
+bot.run('MTEwOTE3NjEzMzI2Nzc3MTU4Mw.GbVN6d.cTgxwHDUaekXvU7CRc9UrfE-6Lg-FgjclO4y1w')
